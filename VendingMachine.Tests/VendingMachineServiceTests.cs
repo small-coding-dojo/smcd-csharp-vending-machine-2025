3147cbd9dfb138a550aee368fe4a698d16e1a43e
Stefan Boos <sboos@igus.net>
2025-05-05 17:53:37 +0200

    feat: When there are no coins inserted, the machine displays INSERT COIN.
    Co-authored-by: Michael Mahlberg <mm@consulting-guild.de>
    Co-authored-by: Stefan Boos <sboos@igus.net>
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Holger Janßen-Kroll <hjanssenkroll@igus.net>
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>

--- VendingMachine.Tests/VendingMachineServiceTests.cs
+++ VendingMachine.Tests/VendingMachineServiceTests.cs
@@ -3,7 +3,38 @@ namespace VendingMachine.Tests;
 public class VendingMachineServiceTests
 {
     [Fact]
-    public void SomeTest()
+    public void FreshMachineShowsInsertCoin()
     {
+        ICanDisplay testDisplay = new TestDisplay();
+        var testMachine = new VendingMachineImplementation(testDisplay);
+        Assert.Equal("INSERT COIN", testDisplay.Output);
     }
 }
+
+public interface ICanDisplay
+{
+    void Show(string insertCoin);
+    string Output { get; set; }
+}
+
+public class VendingMachineImplementation
+{
+    private readonly ICanDisplay _display;
+
+    public VendingMachineImplementation(ICanDisplay display) 
+    {
+        _display = display;
+        _display.Show("INSERT COIN");
+    }
+}
+
+public class TestDisplay : ICanDisplay
+{
+    public string Output { get; set; } = "";
+
+    public void Show(string message)
+    {
+        Output = message;
+    }
+}
+
