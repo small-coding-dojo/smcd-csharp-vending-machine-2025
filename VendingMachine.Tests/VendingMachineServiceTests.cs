10eb8be13e7a5c0c9bc9fbc1402d3e69eeac34cb
Niklas Rueber <nrueber@igus.net>
2025-08-07 17:51:14 +0200

    feat: When there are no coins inserted, the machine displays INSERT COIN.
    Co-authored-by: Stefan Boos <sboos@igus.net>
    Co-authored-by: Niklas Rueber <nrueber@igus.net>

--- VendingMachine.Tests/VendingMachineServiceTests.cs
+++ VendingMachine.Tests/VendingMachineServiceTests.cs
@@ -5,5 +5,27 @@ public class VendingMachineServiceTests
     [Fact]
     public void SomeTest()
     {
+        var testDisplay = new TestDisplay();
+        var subject = new TheVendingMachine(testDisplay);
+        
+        Assert.Equal("INSERT COIN", testDisplay.Output);
+    }
+}
+
+public class TestDisplay
+{
+    public string Output { get; set; } = string.Empty;
+
+    public void Show(string message)
+    {
+        Output = message;
+    }
+}
+
+public class TheVendingMachine
+{
+    public TheVendingMachine(TestDisplay testDisplay)
+    {
+        testDisplay.Show("INSERT COIN");
     }
 }
