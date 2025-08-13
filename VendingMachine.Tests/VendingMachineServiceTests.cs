1392332f9cdb83e0ef7e42fa559c2b3743c4194e
Niklas Rueber <nrueber@igus.net>
2025-08-13 18:03:09 +0200

    feat: we can insert two dimes
    Co-authored-by: Stefan Boos <sboos@igus.net>

--- VendingMachine.Tests/VendingMachineServiceTests.cs
+++ VendingMachine.Tests/VendingMachineServiceTests.cs
@@ -16,19 +16,35 @@ public class VendingMachineServiceTests
     [Fact]
     public void When_inserting_2_nickels_Then_show_0_10()
     {
-        // arrange
         var testDisplay = new TestDisplay();
         var subject = new TheVendingMachine(testDisplay);
-        // act
-        object nickel = new { };
+        
+        var nickel = new PhysicalObject(5.00);
+
         subject.Insert(nickel);
         Assert.Equal("0.05", testDisplay.Output);
+
         subject.Insert(nickel);
-        
-        // assert
         Assert.Equal("0.10", testDisplay.Output);
     }
+
+    [Fact]
+    public void When_inserting_2_dimes_Then_show_0_20()
+    {
+        var testDisplay = new TestDisplay();
+        var subject = new TheVendingMachine(testDisplay);
+        
+        var dime = new PhysicalObject(2.268);
+        
+        subject.Insert(dime);
+        Assert.Equal("0.10", testDisplay.Output);
+
+        subject.Insert(dime);
+        Assert.Equal("0.20", testDisplay.Output);
+    }
 }
+
+public readonly record struct PhysicalObject(double Weight);
 
 public class TestDisplay
 {
@@ -51,9 +67,17 @@ public class TheVendingMachine
         _testDisplay.Show("INSERT COIN");
     }
 
-    public void Insert(object coin)
+    public void Insert(PhysicalObject coin)
     {
-        _balance += 0.05m;
+        if (coin.Weight == 2.268)
+        {
+            _balance += 0.10m;
+        }
+        else
+        {
+            _balance += 0.05m;
+        }
+
         _testDisplay.Show(_balance.ToString(CultureInfo.InvariantCulture));
     }
 }
