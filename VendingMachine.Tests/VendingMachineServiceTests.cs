d9dca0fda5423cc39bdfad71e69145b5928e0184
Niklas Rueber <nrueber@igus.net>
2025-08-25 17:34:38 +0200

    feat:
    relates-to: @1308

--- VendingMachine.Tests/VendingMachineServiceTests.cs
+++ VendingMachine.Tests/VendingMachineServiceTests.cs
@@ -73,8 +73,11 @@ public class TheVendingMachine
             _balance += 0.30m;
         else if (coin.Diameter == 17.91)
             _balance += 0.10m;
-        else if(coin.Diameter >= 21.21 && coin.Diameter <= 21.22)
+        else if (coin.Diameter >= 21.21 && coin.Diameter <= 21.22)
+        {
+            var theCoin = new Nickel();
             _balance += 0.05m;
+        }
         if(_balance > 0.0m)
             Output = _balance.ToString("F2", CultureInfo.InvariantCulture);
     }
